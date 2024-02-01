01ca09383aca0fa112a63d56e47b488ad234f641
the_byte_bender <cccefg2@gmail.com>
2024-02-01 18:25:17 +0300

    fix: Avoid duplicate movements.

--- src/event_handler.ts
+++ src/event_handler.ts
@@ -91,7 +91,10 @@ export default class EventHandler {
             if (!entity && data.id === this.gameplay.player?.id) {
                 entity = this.gameplay.player ?? undefined;
             }
-            if (entity) {
+            if (
+                entity &&
+                !entity.isSamePosition({ x: data.x, y: data.y, z: data.z })
+            ) {
                 entity.move(
                     data.x,
                     data.y,
